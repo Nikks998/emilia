ddfc4ff39b878d7d1f3dbe284bf8e48cc02a3343
Caceres Matias <kcerescmh98@gmail.com>
2023-06-13 12:54:28 -0300

    feat: agrega configuracion recursos estaticos

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app  = express();
 const path = require('path')
 const PORT = 3030
 
+/* configuraciones */
+app.use(express.static('public'))
+
+
 /* Rutas */
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
 app.get('/about', (req,res) => res.sendFile(path.join(__dirname, 'views', 'about.html')))
